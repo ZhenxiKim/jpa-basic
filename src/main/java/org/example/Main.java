c4f75a152797bdc6ff71d5f6eae2a3be50e7b1a8
ZhenxiKIm <8055kjh@gmail.com>
2022-12-14 00:25:31 +0900

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author jhkim
+ * @since $DATE
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
