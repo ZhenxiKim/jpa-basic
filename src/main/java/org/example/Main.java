c7d8c1e92bc3a1d68170bdbb0befaeab148b2963
zhenxiKim <8055kjh@gmail.com>
2022-12-29 17:47:02 +0900

    entityManager CRUD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * @author jhkim
  * @since $DATE
@@ -7,6 +12,32 @@ package org.example;
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hello");
+		EntityManager entityManager = factory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		try{
+			Member member = new Member();
+			member.setId(4L);
+			member.setName("test");
+
+			entityManager.persist(member);//jpa 저장
+			transaction.commit();
+
+			Member findMember = entityManager.find(Member.class, 1L);
+			System.out.println("findMember.getId() = " + findMember.getId());
+			System.out.println("findMember.getName() = " + findMember.getName());
+
+			findMember.setName("hello jpa");
+			transaction.commit();
+			System.out.println("findMember.getName() = " + findMember.getName());
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			entityManager.close();
+			factory.close();
+		}
 	}
 }
